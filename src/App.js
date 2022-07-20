f479a32f5efb2f9356d427ab5ab862bcf15bb093
Jana Jolovic <janajolovic04@gmail.com>
2022-07-20 18:14:55 +0200

    register form, routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/" />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
