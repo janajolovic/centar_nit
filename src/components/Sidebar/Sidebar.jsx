299bcbd5773925fc5f71e7d7e5bd95ba8889b4ee
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-07-30 18:59:44 +0200

    minor changes on sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,34 +7,48 @@ const Sidebar = () => {
   return (
     <div className={classes.sidebar}>
       <h1>Centar NIT</h1>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon="fa-solid fa-house" />
-        <Link to="/">Homepage</Link>
-      </div>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon="fa-solid fa-laptop-code" />
-        <Link to="/courses">Courses</Link>
-      </div>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon="fa-solid fa-bell" />
-        <Link to="/">Notifications</Link>
-      </div>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon="fa-solid fa-calendar" />
-        <Link to="/">News</Link>
-      </div>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon="fa-solid fa-address-card" />
-        <Link to="/">About us</Link>
-      </div>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon="fa-solid fa-user" />
-        <Link to="/">My profile</Link>
-      </div>
-      <div className={classes.icons}>
-        <FontAwesomeIcon icon={["fas", "fa-right-from-bracket"]} size="lg" />
-        <Link to="/">Logout</Link>
-      </div>
+      <Link to="/">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon="fa-solid fa-house" />
+          <span>Homepage</span>
+        </div>
+      </Link>
+      <Link to="/courses">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon="fa-solid fa-laptop-code" />
+          <span>Courses</span>
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon="fa-solid fa-bell" />
+          <span>Notifications</span>
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon="fa-solid fa-calendar" />
+          <span>News</span>
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon="fa-solid fa-address-card" />
+          <span>About us</span>
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon="fa-solid fa-user" />
+          <span>My profile</span>
+        </div>
+      </Link>
+      <Link to="/">
+        <div className={classes.icons}>
+          <FontAwesomeIcon icon={["fas", "fa-right-from-bracket"]} size="lg" />
+          <span>Logout</span>
+        </div>
+      </Link>
     </div>
   );
 };
